a26fa86927efa96bae6f06c98281a375a889378e
David Slezak <david.slezak.s@gyarab.cz>
2026-01-22 18:21:04 +0100

    kurzy

---
+++ kurzy/exchange.py
@@ -0,0 +1,7 @@
+import httpx
+
+r = httpx.get('https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt')
+print(r.text)
+print(r.status)
+lines = r.text.split('\n')
+print(lines[0])
