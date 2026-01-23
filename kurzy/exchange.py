d2dd2ad475a0e07b7db3a1c018d63059795c79de
David-Slezak <david.slezak.s@gyarab.cz>
2026-01-23 10:06:54 +0100

    aaaa

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -2,6 +2,6 @@ import httpx
 
 r = httpx.get('https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt')
 print(r.text)
-print(r.status)
+print(r.status_code)
 lines = r.text.split('\n')
 print(lines[0])
