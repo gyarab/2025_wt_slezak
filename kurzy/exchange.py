0167d00103f7e3560aa3ae8a518fecdcaddccce3
David-Slezak <david.slezak.s@gyarab.cz>
2026-01-23 10:18:12 +0100

    ratin my euro

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -5,3 +5,17 @@ print(r.text)
 print(r.status_code)
 lines = r.text.split('\n')
 print(lines[0])
+line_euro = ""
+
+for line in lines:
+    if "EUR" in line:
+        line_euro = line
+        break
+rate_str = line_euro.split('|')[-1].replace(',','.')
+rate = float(rate_str)
+
+print("kurz je", rate)
+
+value_in = float(input("kolik mas eur?"))
+value_out = value_in * rate
+print(f"Tak to je {value_out:.2f} korun")
